d5cd9a0c5f727cfb39b915293a19b4b4928fdaba
Richard Mamani <richard.mamani@tecsup.edu.pe>
2021-07-01 22:41:22 -0500

    asd

---
+++ app/src/main/java/tecsup/edu/recuperacionintegrador/DetalleViewModel.kt
@@ -0,0 +1,7 @@
+package tecsup.edu.recuperacionintegrador
+
+import androidx.lifecycle.ViewModel
+
+class DetalleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/tecsup/edu/recuperacionintegrador/resource/Lista.kt
@@ -0,0 +1,5 @@
+package tecsup.edu.recuperacionintegrador.resource
+
+data class Lista(
+    val results: MutableList<Result>?
+)
